5ad83da45f6d329cfdb2bf2e89533e7f3522267f
JoaoVictorMoura <95266878+JoaoVMouraDev@users.noreply.github.com>
2026-06-03 16:03:32 -0300

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -22,8 +22,8 @@ import {
   getCommentsByPost,
 } from "./db";
 import { getSessionCookieOptions } from "./_core/cookies.ts";       
-import { systemRouter } from "./_core/systemRouter";
-import { protectedProcedure, publicProcedure, router } from "./_core/trpc";
+import { systemRouter } from "./_core/systemRouter.js";
+import { protectedProcedure, publicProcedure, router } from "./_core/trpc.js";
 import { TRPCError } from "@trpc/server";
 import { nanoid } from "nanoid";
 import { hashPassword, verifyPassword, validatePasswordStrength, validateEmail, validateUsername } from "./auth.ts"; 
